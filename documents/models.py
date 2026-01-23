af67f4b1392f20ba3ae5b18ad20ac8d71bfac511
SRR-GIT <108361901+SRR-GIT@users.noreply.github.com>
2026-01-23 10:25:48 +0100

    Update models.py

--- documents/models.py
+++ documents/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Document(models.Model):
     title = models.CharField(max_length=200)
     file = models.FileField(upload_to="documents/")
+    is_public = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
