26ca26de730a82184a5a5609f3eb302b58c8f9de
SRR-GIT <108361901+SRR-GIT@users.noreply.github.com>
2026-01-23 10:26:24 +0100

    Update views.py

--- documents/views.py
+++ documents/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Document
 
 def document_list(request):
-    docs = Document.objects.order_by("-created_at")
+    docs = Document.objects.filter(is_public=True).order_by("-created_at")
     return render(request, "documents/list.html", {"docs": docs})
