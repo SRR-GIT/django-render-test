72b744b21c55293db96602940f8191f05e870849
SRR-GIT <108361901+SRR-GIT@users.noreply.github.com>
2026-01-27 11:47:22 +0100

    Update views.py

--- documents/views.py
+++ documents/views.py
@@ -7,3 +7,22 @@ def procedure_list(request):
     # MVP: affiche toutes les procédures (on filtrera par établissement ensuite)
     procedures = Procedure.objects.order_by("-updated_at")
     return render(request, "procedures/list.html", {"procedures": procedures})
+
+from django.shortcuts import get_object_or_404
+from .models import Procedure
+
+@login_required
+def procedure_detail(request, pk):
+    procedure = get_object_or_404(Procedure, pk=pk)
+    sections = procedure.sections.all()
+    documents = procedure.documents.all()
+
+    return render(
+        request,
+        "procedures/detail.html",
+        {
+            "procedure": procedure,
+            "sections": sections,
+            "documents": documents,
+        },
+    )
