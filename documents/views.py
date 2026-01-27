5dae9534748538214cb0dc78dcb4b91f5f32372a
SRR-GIT <108361901+SRR-GIT@users.noreply.github.com>
2026-01-27 11:13:21 +0100

    Update views.py

--- documents/views.py
+++ documents/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from .models import Document
+from .models import Procedure
 
-def document_list(request):
-    docs = Document.objects.filter(is_public=True).order_by("-created_at")
-    return render(request, "documents/list.html", {"docs": docs})
+@login_required
+def procedure_list(request):
+    # MVP: affiche toutes les procédures (on filtrera par établissement ensuite)
+    procedures = Procedure.objects.order_by("-updated_at")
+    return render(request, "procedures/list.html", {"procedures": procedures})
