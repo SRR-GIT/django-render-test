79d869c7feca07a6fa29ddf28dd8f41ca0b92c2d
SRR-GIT <108361901+SRR-GIT@users.noreply.github.com>
2026-01-28 14:50:40 +0100

    Update urls.py

--- documents/urls.py
+++ documents/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import procedure_list, procedure_detail
+from . import views
 
 urlpatterns = [
     path("", views.school_list, name="school_list"),
     path("procedures/", views.procedure_list, name="procedure_list"),
     path("procedures/<int:pk>/", views.procedure_detail, name="procedure_detail"),
 ]
+
 
