8b4538d6091eabe029f83bc1df0495144c7d14d5
SRR-GIT <108361901+SRR-GIT@users.noreply.github.com>
2026-01-29 09:56:55 +0100

    Update admin.py

--- documents/admin.py
+++ documents/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import (
-    School,
+    School,SchoolRole,
     ProcedureTemplate, ProcedureTemplateSection,
     Procedure, ProcedureSection,
     ProcedureDocument,
@@ -16,10 +16,16 @@ class ProcedureSectionInline(admin.TabularInline):
     model = ProcedureSection
     extra = 0
     filter_horizontal = ("visible_to_groups",)
+    fields = ("order", "title", "key", "body_html", "visible_to_groups")
 
 class ProcedureDocumentInline(admin.TabularInline):
     model = ProcedureDocument
     extra = 0
+
+class SchoolRoleInline(admin.TabularInline):
+    model = SchoolRole
+    extra = 0
+    filter_horizontal = ("users",)
 
 
 # --- MASQUER LES MODÈLES TECHNIQUES DU MENU ---
@@ -45,6 +51,7 @@ class SchoolAdmin(admin.ModelAdmin):
     list_display = ("name", "commune", "code")
     search_fields = ("name", "commune", "code")
     filter_horizontal = ("groups",)
+    inlines = [SchoolRoleInline]
 
 
 # --- MODÈLES ---
