a12bde5ccde5bce15e3059ad5ddcabfbed273e42
SRR-GIT <108361901+SRR-GIT@users.noreply.github.com>
2026-01-30 14:46:51 +0100

    Create 0003_merge_0002_schoolrole_and_visibility_0002_template_section_visibility.py

---
+++ documents/migrations/0003_merge_0002_schoolrole_and_visibility_0002_template_section_visibility.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("documents", "0002_schoolrole_and_visibility"),
+        ("documents", "0002_template_section_visibility"),
+    ]
+
+    operations = []
