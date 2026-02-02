a0e4e7489a6ad261952a15d5e53212256bc10592
SRR-GIT <108361901+SRR-GIT@users.noreply.github.com>
2026-02-02 12:56:15 +0100

    Update 0004_richtext_fields.py

--- documents/migrations/0004_richtext_fields.py
+++ documents/migrations/0004_richtext_fields.py
@@ -4,8 +4,7 @@ import ckeditor_uploader.fields
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("documents", "0002_schoolrole_and_visibility"),
-        ("documents", "0002_template_section_visibility"),
+        ("documents", "0003_merge_0002_schoolrole_and_visibility_0002_template_section_visibility"),
     ]
 
     operations = [
