eda90cea496ab98ce5c62eb92fd2a44698c0a9bb
SRR-GIT <108361901+SRR-GIT@users.noreply.github.com>
2026-02-03 09:21:52 +0100

    Update admin.py

--- documents/admin.py
+++ documents/admin.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import GroupAdmin
 from django.contrib import admin
+from django import forms
 
 admin.site.unregister(Group)
 @admin.register(Group)
