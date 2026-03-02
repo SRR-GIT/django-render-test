eafa6b9c8f3301fde2237a317ec9019056c2eeda
SRR-GIT <108361901+SRR-GIT@users.noreply.github.com>
2026-03-02 11:58:38 +0100

    Update services.py

--- documents/services.py
+++ documents/services.py
@@ -1,6 +1,6 @@
 from django.db import transaction
 from django.db.models import Max
-from .models import ProcedureVersion, ProcedureSectionVersion
+from .models import ProcedureVersion, ProcedureSectionVersion, Procedure, ProcedureSection
 
 @transaction.atomic
 def create_procedure_version(procedure, user=None, comment=""):
@@ -24,3 +24,27 @@ def create_procedure_version(procedure, user=None, comment=""):
         sv.visible_to_groups.set(s.visible_to_groups.all())
 
     return v
+    
+def create_procedure_from_template(*, school, template, title, user):
+    proc = Procedure.objects.create(
+        school=school,
+        template=template,
+        title=title,
+        status=Procedure.DRAFT,
+        updated_by=user,
+    )
+
+    # Copier les sections du modèle
+    for ts in template.sections.all().order_by("order", "id"):
+        ps = ProcedureSection.objects.create(
+            procedure=proc,
+            title=ts.title,
+            key=ts.key,
+            order=ts.order,
+            body_html=ts.body_html,
+        )
+        # copier les droits
+        ps.visible_to_groups.set(ts.visible_to_groups.all())
+        ps.editable_by_groups.set(ts.editable_by_groups.all())
+
+    return proc
