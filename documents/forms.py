1670b65f4de2291e2ebc8edd16eb70625f1d6543
SRR-GIT <108361901+SRR-GIT@users.noreply.github.com>
2026-03-02 13:46:14 +0100

    Update forms.py

--- documents/forms.py
+++ documents/forms.py
@@ -1,8 +1,13 @@
 from django import forms
-from .models import ProcedureTemplate
+from .models import ProcedureTemplate, ProcedureSection
 
 class ProcedureCreateForm(forms.Form):
     title = forms.CharField(max_length=200)
     template = forms.ModelChoiceField(
         queryset=ProcedureTemplate.objects.filter(is_active=True).order_by("title")
     )
+
+class ProcedureSectionEditForm(forms.ModelForm):
+    class Meta:
+        model = ProcedureSection
+        fields = ["body_html"]  # ou ["title", "body_html"]
