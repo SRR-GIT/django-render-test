323a70643a7df79c61620777aeeb959fc93ea76c
SRR-GIT <108361901+SRR-GIT@users.noreply.github.com>
2026-03-17 07:04:29 +0100

    Update forms.py

--- documents/forms.py
+++ documents/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from ckeditor.widgets import CKEditorWidget
 from .models import ProcedureTemplate, ProcedureSection
 
 class ProcedureCreateForm(forms.Form):
@@ -10,4 +11,9 @@ class ProcedureCreateForm(forms.Form):
 class ProcedureSectionEditForm(forms.ModelForm):
     class Meta:
         model = ProcedureSection
-        fields = ["body_html"]  # ou ["title", "body_html"]
+        fields = ["body_html"]
+
+    body_html = forms.CharField(
+        required=False,
+        widget=CKEditorWidget(attrs={"style": "width: 100%; min-height: 300px;"}),
+    )
