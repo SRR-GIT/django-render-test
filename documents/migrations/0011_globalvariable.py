dd2a32a533705bad201889cb12f5600c1c3a6118
SRR-GIT <108361901+SRR-GIT@users.noreply.github.com>
2026-09-14 14:26:07 +0200

    Update 0011_globalvariable.py

--- documents/migrations/0011_globalvariable.py
+++ documents/migrations/0011_globalvariable.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("documents", "0009_nom_de_ta_derniere_migration"),
+        ("documents", "0010_section_variable.py"),
     ]
 
     operations = [
